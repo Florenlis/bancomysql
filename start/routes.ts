f7309765a30f92a4a4ab01a74b0f898e1957cb6b
Florenlis <jpfloren160@gmail.com>
2023-09-22 08:36:52 -0300

    semipronto

--- start/routes.ts
+++ start/routes.ts
@@ -4,3 +4,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.post("/register", "AuthController.register")
+Route.post("/login", "AuthController.login")
